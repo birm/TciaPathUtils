442d24060c2fb786f7e66ee5f6afbeb06e813bd9
Ryan Birmingham <birm@rbirm.us>
2020-07-23 12:50:38 -0400

    not html

--- kong_reporter/parser.py
+++ kong_reporter/parser.py
@@ -6,18 +6,20 @@ sizes = {}
 hits = {}
 
 for line in sys.stdin:
-    if 'q' == line.rstrip():
-        break
-    l = line.split(" ")
-    if len(l) > 9  and l[5] == '"GET':
-        a = datetime.datetime.strptime(l[3], '[%d/%b/%Y:%H:%M:%S')
-        if(datetime.datetime.now() - a < datetime.timedelta(days=30)):
-            prevSize = sizes.get(l[6].split("?")[0],0)
-            prevHits = hits.get(l[6].split("?")[0],0)
-            sizes[l[6].split("?")[0]] = prevSize + int(l[9])
-            hits[l[6].split("?")[0]] = prevHits + 1
-print("<h1>Kong Log Report for last 30 days</h1>")
-print("<h2> Sizes per Endpoint </h2>")
+  if 'q' == line.rstrip():
+    break
+  l = line.split(" ")
+  if len(l) > 9 and l[5] == '"GET':
+    a = datetime.datetime.strptime(l[3], '[%d/%b/%Y:%H:%M:%S')
+    if (datetime.datetime.now() - a < datetime.timedelta(days = 30)):
+      prevSize = sizes.get(l[6].split("?")[0], 0)
+      prevHits = hits.get(l[6].split("?")[0], 0)
+      sizes[l[6].split("?")[0]] = prevSize + int(l[9])
+      hits[l[6].split("?")[0]] = prevHits + 1
+
+print("*Kong Log Report for last 30 days*")
+print("Total bytes sent to client per Endpoint:")
 print(sizes)
-print("<h2> Hits per Endpoint </h2>")
+print("\n")
+print("Total hits per Endpoint:")
 print(hits)
