9bfa51a87512c3abb4f205762a14c445fc868c22
Ryan Birmingham <birm@rbirm.us>
2020-07-23 12:44:40 -0400

    minor fix

--- kong_reporter/parser.py
+++ kong_reporter/parser.py
@@ -8,13 +8,14 @@ hits = {}
 for line in sys.stdin:
     if 'q' == line.rstrip():
         break
-    if line.split(" ")[5] == '"GET':
-        a = datetime.datetime.strptime(line.split(" ")[3], '[%d/%b/%Y:%H:%M:%S')
+    l = line.split(" ")
+    if len(l) > 9  and l[5] == '"GET':
+        a = datetime.datetime.strptime(l[3], '[%d/%b/%Y:%H:%M:%S')
         if(datetime.datetime.now() - a < datetime.timedelta(days=30)):
-            prevSize = sizes.get(line.split(" ")[6].split("?")[0],0)
-            prevHits = hits.get(line.split(" ")[6].split("?")[0],0)
-            sizes[line.split(" ")[6].split("?")[0]] = prevSize + int(line.split(" ")[9])
-            hits[line.split(" ")[6].split("?")[0]] = prevHits + 1
+            prevSize = sizes.get(l[6].split("?")[0],0)
+            prevHits = hits.get(l[6].split("?")[0],0)
+            sizes[l[6].split("?")[0]] = prevSize + int(l[9])
+            hits[l[6].split("?")[0]] = prevHits + 1
 print("<h1>Kong Log Report for last 30 days</h1>")
 print("<h2> Sizes per Endpoint </h2>")
 print(sizes)
